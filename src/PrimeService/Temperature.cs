2055971c9ec4db2022db12b10cd252def2508b73
Paulo Luiz Fachini <paulo.fachini@pagar.me>
2022-11-11 14:47:01 -0300

    ajustão 3 a vingança

---
+++ src/PrimeService/Temperature.cs
@@ -0,0 +1,12 @@
+namespace Prime.Services
+{
+    public class Temperature
+    {
+        public static class ConversorTemperatura
+        {
+            public static double FahrenheitParaCelsius(double temperatura)
+                //=> (temperatura - 32) / 1.8; // Simulação de falha
+                => Math.Round((temperatura - 32) / 1.8, 2);
+        }
+    }
+}
