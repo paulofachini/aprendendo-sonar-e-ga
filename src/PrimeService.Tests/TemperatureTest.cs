37c81660d5a0d2ed708f81a7714927fbd991f8ec
Paulo Luiz Fachini <paulo.fachini@pagar.me>
2022-11-18 09:41:21 -0300

    ajuste da classe

---
+++ src/PrimeService.Tests/TemperatureTest.cs
@@ -0,0 +1,22 @@
+using static Prime.Services.Temperature;
+
+namespace Prime.UnitTests.Services
+{
+    public class TemperatureTest
+    {
+        [Theory]
+        [InlineData(32, 0)]
+        [InlineData(47, 8.33)]
+        [InlineData(86, 30)]
+        [InlineData(90.5, 32.5)]
+        [InlineData(120.18, 48.99)]
+        [InlineData(212, 100)]
+        public void TestarConversaoTemperatura(
+            double fahrenheit, double celsius)
+        {
+            double valorCalculado =
+                ConversorTemperatura.FahrenheitParaCelsius(fahrenheit);
+            Assert.Equal(celsius, valorCalculado);
+        }
+    }
+}
