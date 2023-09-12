a4e70cbdfdde402fd325ab845a8aacf1674e81aa
Paulo Luiz Fachini <paulofachini@gmail.com>
2023-09-12 13:23:37 -0300

    Update PrimeService.cs
    Apontamento Sonar:
    Member 'IsPrime' does not access instance data and can be marked as static

--- src/PrimeService/PrimeService.cs
+++ src/PrimeService/PrimeService.cs
@@ -2,7 +2,7 @@
 {
     public class PrimeService
     {
-        public bool IsPrime(int candidate)
+        public static bool IsPrime(int candidate)
         {
             if (candidate < 2)
             {
